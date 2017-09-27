791101ce94aa4332cfd2e4169e3425d1fe06e87f
Hsu <hsuxu820@gmail.com>
2017-09-27 11:28:08 +0800

    auto download cudnn

--- 16.04/deb/install_cuda8-1604.sh
+++ 16.04/deb/install_cuda8-1604.sh
@@ -1,6 +1,6 @@
 # writed by Hsu
 # Hsuxu820@gmail.com
-echo '本脚本适用ubuntu 16.04，一键安装cuda-8.0和cudnn，并添加环境变量\nPS：使用本脚本需联网并自行先下载cuda-8.0 和cudnn'
+echo '本脚本适用ubuntu 16.04，一键安装cuda-8.0和cudnn，并添加环境变量\nPS：使用本脚本需联网并自行先下载cuda-8.0,cudnn可自动下载'
 read -p 'Specify path of cuda-8.0 with .deb extension [default=./cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64.deb]:' cuda
 cuda=${cuda:-./cuda-repo-ubuntu1604-8-0-local-ga2_8.0.61-1_amd64.deb}  
 sudo dpkg -i $cuda
@@ -14,10 +14,12 @@ sudo dpkg -i $cublas
 echo 'installing '$cublas
 #sudo apt-get install cublas
 
-read -p 'Specify the path of cudnn with .tgz extension [default=./cudnn-8.0-linux-x64-v5.1.tgz]:' cudnn
-cudnn=${cudnn:-./cudnn-8.0-linux-x64-v5.1.tgz} 
+# read -p 'Specify the path of cudnn with .tgz extension [default=./cudnn-8.0-linux-x64-v5.1.tgz]:' cudnn
+# cudnn=${cudnn:-./cudnn-8.0-linux-x64-v5.1.tgz} 
+CUDNN_URL="http://developer.download.nvidia.com/compute/redist/cudnn/v5.1/cudnn-8.0-linux-x64-v5.1.tgz"
 echo 'extract '$cudnn 'to cuda...'
-tar -xvf $cudnn
+wget -c ${CUDNN_URL}
+tar -xvf cudnn-8.0-linux-x64-v5.1.tgz
 sudo cp cuda/include/* /usr/local/cuda-8.0/include/
 sudo cp cuda/lib64/libcudnn* /usr/local/cuda-8.0/lib64/
 sudo ldconfig
